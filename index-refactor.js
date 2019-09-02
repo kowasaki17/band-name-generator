e446d8913456fab78f0a555ac017b4e15bf9d959
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-09-02 15:44:40 +0200

    Changed store to array

--- index-refactor.js
+++ index-refactor.js
@@ -1,15 +1,24 @@
+const arrBandNames = [];
+
 function generateBandName(clothingColour, lastFoodEaten){
 
   let clothingColorFirst = clothingColour.charAt(0).toUpperCase();
-  let clothingColorRest =  clothingColour.substr(1).toLowerCase();
+  let clothingColorRest = clothingColour.substr(1).toLowerCase();
   let lastFoodEatenFirst = lastFoodEaten.charAt(0).toUpperCase();
   let lastFoodEatenRest = lastFoodEaten.substr(1).toLowerCase();
 
-    return `The ${clothingColorFirst}${clothingColorRest} ${lastFoodEatenFirst}${lastFoodEatenRest}`;
+  let color = `${clothingColorFirst}${clothingColorRest}`;
+  let food = `${lastFoodEatenFirst}${lastFoodEatenRest}`;
+
+  let bandName = `The ${color} ${food}`;
+
+  arrBandNames.push(bandName);
+
+  console.log(arrBandNames);
+
+    return bandName;
 
 }
-let bandName = generateBandName('black', 'spaghetti');
-let arrBandName = [bandName];
 
-console.log(arrBandName);
-console.log(bandName);
+
+console.log(generateBandName('green', 'sausage'));
