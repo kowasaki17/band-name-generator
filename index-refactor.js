085b438e65a1a81c0f07e1bb450081eab1ea0bbd
kowasaki17 <herrmann@gebrauchsgrafiker.de>
2019-09-03 14:11:22 +0200

    Two functions beginning

--- index-refactor.js
+++ index-refactor.js
@@ -1,24 +1,28 @@
 const arrBandNames = [];
 
-function generateBandName(clothingColour, lastFoodEaten){
+function generateClothingColor(clothingColour){
 
   let clothingColorFirst = clothingColour.charAt(0).toUpperCase();
   let clothingColorRest = clothingColour.substr(1).toLowerCase();
+
+  let color = `${clothingColorFirst}${clothingColorRest}`;
+
+    return color;
+
+}
+function generateFoodName(lastFoodEaten){
+
   let lastFoodEatenFirst = lastFoodEaten.charAt(0).toUpperCase();
   let lastFoodEatenRest = lastFoodEaten.substr(1).toLowerCase();
 
-  let color = `${clothingColorFirst}${clothingColorRest}`;
   let food = `${lastFoodEatenFirst}${lastFoodEatenRest}`;
 
-  let bandName = `The ${color} ${food}`;
-
-  arrBandNames.push(bandName);
-
-  console.log(arrBandNames);
-
-    return bandName;
-
+  return food;
 }
 
+//let bandName = `The ${color} ${food}`;
+//arrBandNames.push(bandName);
+//console.log(arrBandNames);
 
-console.log(generateBandName('green', 'sausage'));
+console.log(generateClothingColor('green'));
+console.log(generateFoodName('sausage'));
